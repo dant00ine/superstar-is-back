c52fcbdb8af61a2cda2c18496f2d0133935fd993
Daniel Thompson <danthwa@gmail.com>
2017-04-24 13:41:24 -0700

    adds authenticable module for managing authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  #because APIs don't have sessions
   protect_from_forgery with: :null_session
+
+  # app/controllers/concerns/authenticable.rb
+  include Authenticable
+
 end
