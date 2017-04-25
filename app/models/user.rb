aba1a866376b23927e5accf88e93df420b5fb7ea
Daniel Thompson <danthwa@gmail.com>
2017-04-25 15:04:35 -0700

    removed horrible devise becuase yuck

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+  has_secure_password
   validates :auth_token, uniqueness: true
   validates :username, presence: true, uniqueness: true
 
